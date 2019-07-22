3caf67082f414a3dd0e02b56308bb34866892e6e
Yota Shimoda <shimoda@weseek.co.jp>
2019-07-22 09:39:37 +0900

    第二引数を変更

--- return_alphabet.py
+++ return_alphabet.py
@@ -1,7 +1,7 @@
 def return_alphabet(i,a = None):
     list_upcase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     list_downcase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    if a == 'A':
+    if a == 'u':
         return list_upcase[i]
     else:
         return list_downcase[i]
